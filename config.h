1039b8876633d94a5f5fe4f8425b128f46b4b016
Aistis Raulinaitis <sheganinans@gmail.com>
2015-05-13 01:41:19 -0700

    init

---
+++ config.h
@@ -0,0 +1,47 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Defined if compiling with mingw */
+/* #undef CYGWIN */
+
+/* Defined if a debugging version is to be built */
+/* #undef DEBUG */
+
+/* Path to ghc libraries */
+#define GHC_LIB_PATH "/nix/store/04742sjsszlpijv9r741yrssdhq2l6f8-ghc-7.10.1/lib/ghc-7.10.1"
+
+/* Define to 1 if you have the `arc4random' function. */
+/* #undef HAVE_ARC4RANDOM */
+
+/* Whether symbols are prefixed with a leading underscore */
+#define LEADING_UNDERSCORE 0
+
+/* Defined if compiling on the mac */
+/* #undef MACOSX */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Path to top of build tree */
+#define TOP "/home/a2/Projects/tradeStation/Haskell/plugins-1.5.4.0"
+
+/* Which ghc to use */
+#define WITH_GHC "ghc"
+
+/* Defined if hs-plugins is to be built with Niklas Broberg's HSX parser */
+/* #undef WITH_HSX */
+
+/* Defined if compiling with mingw */
+/* #undef __MINGW32__ */
---
+++ config.mk
@@ -0,0 +1,4 @@
+default: all
+TOP             = /home/a2/Projects/tradeStation/Haskell/plugins-1.5.4.0
+GHC             = ghc
+GLASGOW_HASKELL = 710
